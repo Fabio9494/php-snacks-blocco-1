1b1ac65e5d4fe106dd6fd0fc56a6472ccfb281c7
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-24 18:39:25 +0100

    add snack 1

--- snack_1/index.php
+++ snack_1/index.php
@@ -1,3 +1,37 @@
+<?php $matches = [
+    [
+        'squadra_casa' => 'trentino',
+        'squadra_ospite' => 'brescia',
+        'punti_casa' => 78,
+        'punti_ospiti' => 85
+    ],
+    [
+        'squadra_casa' => 'pistoia',
+        'squadra_ospite' => 'scafati',
+        'punti_casa' => 78,
+        'punti_ospiti' => 85
+    ],
+    [
+        'squadra_casa' => 'brindisi',
+        'squadra_ospite' => 'napoli',
+        'punti_casa' => 78,
+        'punti_ospiti' => 85
+    ],
+    [
+        'squadra_casa' => 'varese',
+        'squadra_ospite' => 'bologna',
+        'punti_casa' => 78,
+        'punti_ospiti' => 85
+    ],
+    [
+        'squadra_casa' => 'tortona',
+        'squadra_ospite' => 'venezia',
+        'punti_casa' => 78,
+        'punti_ospiti' => 85
+    ]
+    ];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +40,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php foreach ($matches as $match => $risultato_partita) {
+                echo $risultato_partita['squadra_casa'] . " - " . $risultato_partita['squadra_ospite'] . " | " . $risultato_partita['punti_casa'] . "-" . $risultato_partita['punti_ospiti'];
+                echo "<br>";
+           } ?>
 </body>
 </html>
