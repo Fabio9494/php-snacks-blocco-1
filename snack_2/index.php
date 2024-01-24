2907d6cf5f35737ba87787a7d1c0c9eea061ae79
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-24 19:00:53 +0100

    add snack 2

--- snack_2/index.php
+++ snack_2/index.php
@@ -1,3 +1,9 @@
+<?php
+  $name = $_GET['name'];
+  $mail = $_GET['mail'];
+  $age = $_GET['age'];
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +12,36 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="./index.php" method="GET">
+                    <div class="col-6">
+                        <label for="control-label">NOME</label>
+                        <input type="text" id="nome" class="form-control" name="name" required>
+                    </div>
+                    <div class="col-6">
+                        <label for="control-label">EMAIL</label>
+                        <input type="text" id="email" class="form-control" name="mail" required>
+                    </div>
+                    <div class="col-6">
+                        <label for="control-label">ETA'</label>
+                        <input type="text" id="age" class="form-control" name="age" required>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="my-2">Invia</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php 
+       if (strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
+        echo "<div>ACCESSO RIUSCITO</div>";
+      } else {
+        echo "<div>ACCESSO NEGATO</div>";
+      }
+    ?>
 </body>
 </html>
